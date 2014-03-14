fdde2e41ad5994aeddfe2a558fa1f90ba999aa2a
Avraam Tsantekidis <avraam.tsantekidis@cern.ch>
2014-03-14 17:46:42 +0100

    Initial commit

--- modules/bibformat/lib/elements/bfe_authority_links.py
+++ modules/bibformat/lib/elements/bfe_authority_links.py
@@ -29,7 +29,21 @@ from invenio.bibauthority_engine import \
     get_low_level_recIDs_from_control_no, \
     get_dependent_records_for_control_no
 
+
 __revision__ = "$Id$"
+
+from urllib2 import urlopen
+from lxml import etree
+def get_wikipedia_link(viaf_id):
+    url = "http://viaf.org/viaf/" + str(viaf_id) +"/viaf.xml"
+    string_xml = urlopen(url).read()
+    xml = etree.fromstring(str(string_xml))
+    author_wikipedia_id = xml.xpath("/ns2:VIAFCluster/ns2:sources/ns2:source[contains(text(),'WKP')]/@nsid",namespaces={"ns2":"http://viaf.org/viaf/terms#"})
+    if type(author_wikipedia_id) is list:
+        author_wikipedia_id = author_wikipedia_id[0]
+    url_to_wikipedia = "http://www.wikipedia.com/wiki/"+author_wikipedia_id
+    return url_to_wikipedia
+
 
 def format_element(bfo):
     """ Prints the control number of an author authority record in HTML.
